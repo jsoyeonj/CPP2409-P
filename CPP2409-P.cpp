1b117d463736de41f48639f93b16910dfd75a9a4
jsoyeonj <jhhhanna@naver.com>
2024-11-13 11:23:41 +0900

    CPP2409-P

--- CPP2409-P.cpp
+++ CPP2409-P.cpp
@@ -120,88 +120,6 @@ public:
             {0, 0, 0, 0, 0}};
     // 블록 모양을 배열로 관리하는 BlockShapes 멤버 변수 선언
     int (*BlockShapes[7])[5] = {IBlock, OBlock, TBlock, ZBlock, SBlock, LBlock, JBlock};
-    void DrawNextBlock(int blockShape[5][5]) // 다음 블록 출력
-    {
-        for (int i = 0; i < 6; i++)
-        {
-            for (int j = 0; j < 6; j++)
-            {
-                gotoxy(57 + j * 2, 6 + i);
-                cout << "  ";
-            }
-        }
-        switch (BlockInfo)
-        {
-        case BlockI:
-            for (int i = 0; i < 4; i++)
-            {
-                gotoxy(62, 8 + i);
-                cout << "■";
-            }
-            break;
-        case BlockO:
-            for (int i = 0; i < 2; i++)
-                for (int j = 0; j < 2; j++)
-                {
-                    gotoxy(61 + j * 2, 8 + i);
-                    cout << "■";
-                }
-            break;
-        case BlockT:
-            for (int j = 0; j <= 2; j++)
-            {
-                gotoxy(60 + j * 2, 8);
-                cout << "■";
-            }
-            gotoxy(62, 9);
-            cout << "■";
-            break;
-        case BlockZ:
-            for (int j = 0; j < 2; j++)
-            {
-                gotoxy(61 + j * 2, 9);
-                cout << "■";
-                gotoxy(61 + j * 2 + 2, 10);
-                cout << "■";
-            }
-            break;
-        case BlockS:
-            for (int j = 0; j < 2; j++)
-            {
-                gotoxy(63 + j * 2, 9);
-                cout << "■";
-                gotoxy(63 + j * 2 - 2, 10);
-                cout << "■";
-            }
-            break;
-        case BlockL:
-            for (int i = 0; i < 3; i++)
-            {
-                gotoxy(61, 8 + i);
-                cout << "■";
-                if (i == 2)
-                {
-                    gotoxy(63, 8 + i);
-                    cout << "■";
-                }
-            }
-            break;
-        case BlockJ:
-            for (int i = 0; i < 3; i++)
-            {
-                gotoxy(63, 8 + i);
-                cout << "■";
-                if (i == 2)
-                {
-                    gotoxy(61, 8 + i);
-                    cout << "■";
-                }
-            }
-            break;
-        default:
-            break;
-        }
-    }
 
     int (*currentBlock)[5];
     Position blockPosition;
@@ -253,15 +171,28 @@ void InitMap(char map[MAPHEIGHT][MAPWIDTH])
     }
 }
 
-void DrawMap(char map[MAPHEIGHT][MAPWIDTH])
+void DrawMap(char map[MAPHEIGHT][MAPWIDTH]) // 맵 생성
 {
-    system("cls");
+    // 1. 30x15 맵을 에워싸는 틀 생성 및 출력
+    gotoxy(10, 5);
+    for (int i = 0; i <= MAPHEIGHT; i++)
+    {
+        for (int j = 0; j < MAPWIDTH + 2; j++)
+        {
+            gotoxy(10 + j * 2, 5 + i);
+            if (j == MAPWIDTH + 1 || j == 0 || i == MAPHEIGHT)
+                cout << "▩";
+        }
+    }
+    // 2. 틀 안에 30x15 맵 생성 및 출력
+    gotoxy(12, 5);
     for (int i = 0; i < MAPHEIGHT; i++)
     {
         for (int j = 0; j < MAPWIDTH; j++)
         {
+            gotoxy(12 + j * 2, 5 + i);
             if (map[i][j] == '0')
-                cout << "  "; // 빈 공간을 두 칸으로 수정
+                cout << "  ";
             else if (map[i][j] == '1')
                 cout << "■";
         }
@@ -283,6 +214,7 @@ void Play()
         Sleep(500);
     }
 }
+
 int main()
 {
     SetConsoleEncoding(); // 콘솔 인코딩 설정
